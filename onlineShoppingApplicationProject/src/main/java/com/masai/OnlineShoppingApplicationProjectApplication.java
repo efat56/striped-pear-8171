202fef6b09f36e3730eec4c00e617195019114a3
efat56 <104213039+efat56@users.noreply.github.com>
2022-11-09 19:18:34 +0530

    day2

---
+++ onlineShoppingApplicationProject/src/main/java/com/masai/OnlineShoppingApplicationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingApplicationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingApplicationProjectApplication.class, args);
+	}
+
+}
