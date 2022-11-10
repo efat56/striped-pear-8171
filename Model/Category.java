473b5bea13adcb5dccc8488dbeb1d6d77c0de275
Rupak Tyagi <rupaktyagi21@gmail.com>
2022-11-10 11:39:55 +0530

    Add files via upload

---
+++ Model/Category.java
@@ -0,0 +1,29 @@
+package com.masai.Model;
+
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer categoryID;
+	
+	
+	@NotNull(message = "Category name can not be Null")
+	@Size(min = 3 ,message = "Please Enter valid category")
+	private String categoryName;
+	
+	
+}
