b3eaab65d47164579b448add13180610945e8d55
spvhantale <vhantale510@gmail.com>
2022-11-13 13:22:56 +0530

    completed project final

--- onlineShoppingApplication/src/main/java/com/masai/repository/OrdersRepo.java
+++
@@ -1,9 +0,0 @@
-package com.masai.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.masai.model.Orders;
-
-public interface OrdersRepo extends JpaRepository<Orders, Integer> {
-
-}
